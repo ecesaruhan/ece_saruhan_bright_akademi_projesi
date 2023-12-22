f2a3cf75361f667d38a4bc6a9c8b0fe35ae8fea0
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2023-12-22 20:05:31 +0300

    Header-navbar

--- bright-academie-frontend-project/src/Components/Header.js
+++ bright-academie-frontend-project/src/Components/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const Header = () => {
-    return (
-      
-    <div>Header</div>
+  return (
+    <>
+      <nav id='header-nav' className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand text-white" href="/">Bright Dance Studio</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarToggler">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Trainings</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Trainers</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
